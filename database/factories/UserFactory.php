23782725401063513940f4cd2472a9c10eb68409
Harun1804 <Harun.arrasyid1804@gmail.com>
2022-02-08 14:11:21 +0700

    Update Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,11 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
@@ -21,19 +16,5 @@ class UserFactory extends Factory
             'password' => Hash::make('harun123'), // password
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
