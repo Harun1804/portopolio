5585a6bd7aacfc40601a73f3a9d2e94ca569afcd
Harun1804 <Harun.arrasyid1804@gmail.com>
2022-02-08 14:13:09 +0700

    Fix Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -22,6 +25,13 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            'name' => 'harun',
+            'email' => 'aegistar1804@gmail.com',
+            'password' => Hash::make('harun123'), // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 
     /**
